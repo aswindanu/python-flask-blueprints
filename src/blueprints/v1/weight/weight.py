62dc1e2b4b91202f5e0496c0b7d2ecab90cf0e6b
aswindanu <aswindanu.prihastomo@gmail.com>
2024-06-25 16:38:56 +0700

    simplofy

--- src/blueprints/v1/weight/weight.py
+++ src/blueprints/v1/weight/weight.py
@@ -6,6 +6,7 @@ from flask import Blueprint, redirect
 from flask_restful import Resource, Api, reqparse, marshal, inputs, request
 from sqlalchemy import asc, desc, and_, or_
 
+from internal.service.crud import ParentResource
 from infrastructure.model.db_model import db, Weight
 from src import success_template, error_template
 
@@ -14,7 +15,7 @@ bp_weight = Blueprint('weight', __name__)
 api = Api(bp_weight)
 
 
-class HomeResource(Resource):
+class HomeResource(ParentResource):
     def get(self):
         qry = Weight.query.order_by(desc(Weight.date))
         results = []
@@ -37,7 +38,7 @@ class HomeResource(Resource):
             'date': 'Mean',
             'weight': f'{round(mean_weight, 2)}',
             'loss': f'{round(mean_loss, 2)}',
-            'status': 'GO',
+            'status': '-',
         })
         return success_template('home.html', results)
 
@@ -45,7 +46,7 @@ class HomeResource(Resource):
         return {}, 200
 
 
-class DetailResource(Resource):
+class DetailResource(ParentResource):
     def get(self):
         parser = reqparse.RequestParser()
         parser.add_argument('id', location='args', required=True)
@@ -57,7 +58,7 @@ class DetailResource(Resource):
         return {}, 200
 
 
-class CrudResource(Resource):
+class CrudResource(ParentResource):
     def get(self):
         parser = reqparse.RequestParser()
         parser.add_argument('id', location='args')
@@ -69,19 +70,19 @@ class CrudResource(Resource):
 
     def post(self):
         if not request.form['weight'] or not request.form['date']:
-            return error_template('home.html', [], 'All forms should be filled', 400)
+            return error_template('error.html', [], 'All forms should be filled', 400)
         try:
             date = datetime.strptime(request.form['date'], "%Y-%m-%d")
         except Exception:
-            return error_template('home.html', [], 'Data date should be date', 400)
+            return error_template('error.html', [], 'Data date should be date', 400)
         try:
             weight = int(request.form['weight'])
-            qry = Weight.query.filter(Weight.date <= date).order_by(desc(Weight.date)).first()
+            qry = Weight.query.filter(Weight.date < date).order_by(desc(Weight.date)).first()
             loss = 0
             if qry:
                 loss = qry.weight - weight
         except Exception:
-            return error_template('home.html', [], 'Data weight should be numeric', 400)
+            return error_template('error.html', [], 'Data weight should be numeric', 400)
         status = None
         for k, v in Weight.WEIGHT_STATUS.items():
             if weight < k:
@@ -92,7 +93,7 @@ class CrudResource(Resource):
             db.session.add(data)
             db.session.commit()
         except Exception as e:
-            return error_template('home.html', [], 'Data \'ID\' already exist (%s)' % e, 400)
+            return error_template('error.html', [], 'Data \'ID\' already exist (%s)' % e, 400)
         return redirect(f"/weight/detail?id={data.id}")
 
     def options(self):
@@ -102,20 +103,20 @@ class CrudResource(Resource):
 class EditResource(CrudResource):
     def post(self):
         if not request.form['weight'] or not request.form['date']:
-            return error_template('edit.html', [], 'All forms should be filled', 400)
+            return error_template('error.html', [], 'All forms should be filled', 400)
         qry = Weight.query.get(request.form["id"])
-        try:
-            weight = int(request.form['weight'])
-            qry = Weight.query.filter(Weight.date <= date).order_by(desc(Weight.date)).first()
-            loss = 0
-            if qry:
-                loss = qry.weight - weight
-        except Exception:
-            return error_template('edit.html', [], 'Data weight should be numeric', 400)
         try:
             date = datetime.strptime(request.form['date'], "%Y-%m-%d")
         except Exception:
-            return error_template('edit.html', [], 'Data date should be date', 400)
+            return error_template('error.html', [], 'Data date should be date', 400)
+        try:
+            weight = int(request.form['weight'])
+            qry = Weight.query.filter(and_(Weight.date < date)).order_by(desc(Weight.date)).first()
+            loss = 0
+            if qry:
+                loss = qry.weight - weight
+        except Exception as e:
+            return error_template('error.html', [], 'Data weight should be numeric (%s)' % e, 400)
         status = None
         for k, v in Weight.WEIGHT_STATUS.items():
             if weight < k:
@@ -129,12 +130,12 @@ class EditResource(CrudResource):
             qry.date = date
             db.session.commit()
         except Exception as e:
-            return error_template('edit.html', [], 'Data failed to save (%s)' % e, 500)
+            return error_template('error.html', [], 'Data failed to save (%s)' % e, 500)
         return redirect(f"/weight/detail?id={qry.id}")
 
 
 base_uri = "/weight"
 api.add_resource(HomeResource, base_uri + '/home', base_uri + '/home')
-api.add_resource(DetailResource, base_uri + '/detail', base_uri + '/detail')
 api.add_resource(CrudResource, base_uri + '/create', base_uri + '/create')
+api.add_resource(DetailResource, base_uri + '/detail', base_uri + '/detail')
 api.add_resource(EditResource, base_uri + '/edit', base_uri + '/edit')
